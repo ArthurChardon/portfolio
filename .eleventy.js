d68c0eaa1ef11ede5ff8acf569e40d5bcc13ffe7
Arthur Chardon <arthur.chardon74@gmail.com>
2025-03-19 17:38:56 +0100

    init: init web page with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/styles.css");
+    eleventyConfig.addPassthroughCopy("assets/Slabo13px-Regular.ttf");
+    eleventyConfig.addPassthroughCopy("assets/Montserrat-VariableFont_wght.ttf");
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            includes: "_includes",
+        }
+    };
+};
---
+++ src/index.md
@@ -0,0 +1,5 @@
+---
+title: Arthur Chardon
+layout: "base.njk"
+---
+<h2>Computer science engineer, web developer & tech savyy</h2>
