81c5133c1edb44f87f4b019aa101963bb064e85a
Arthur Chardon <arthur.chardon74@gmail.com>
2025-03-21 16:03:00 +0100

    big title, hoverable subtitle, typing description, icons stack

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles.css");
-    eleventyConfig.addPassthroughCopy("src/custom.css");
-    eleventyConfig.addPassthroughCopy("assets/Slabo13px-Regular.ttf");
-    eleventyConfig.addPassthroughCopy("assets/Montserrat-VariableFont_wght.ttf");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("assets");
     return {
         dir: {
             input: "src",
