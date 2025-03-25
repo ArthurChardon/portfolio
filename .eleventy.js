37a2e42320b64748ec4dbc6a446b1e0ab6a45759
Arthur Chardon <arthur.chardon74@gmail.com>
2025-03-25 10:16:29 +0100

    misc: add and ran prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/styles.css");
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("assets");
-    return {
-        dir: {
-            input: "src",
-            output: "dist",
-            includes: "_includes",
-        }
-    };
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("assets");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes",
+    },
+  };
 };
